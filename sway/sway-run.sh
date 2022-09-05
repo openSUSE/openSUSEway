d2e3d2dfb0a1bc5c83d58c944ee5bcff46e41777
Filippo Bonazzi <filippo.bonazzi@suse.com>
2022-09-05 13:02:44 +0200

    Import user configuration variables
    GDM and KDE Plasma source systemd user environment variables from
    ~/.config/environment.d/, sway does not.
    Let's provide this functionality while we're at it.

--- sway/sway-run.sh
+++ sway/sway-run.sh
@@ -5,9 +5,11 @@ export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
 export XDG_CURRENT_DESKTOP=sway
 
-# this file imports openSUSEway desktop enviroments
 set -a
+# Import openSUSEway environment variables
 . /etc/sway/env
+# Import user environment variables
+. ~/.config/environment.d/*.conf
 set +a
 
 systemd-cat --identifier=sway sway $@
